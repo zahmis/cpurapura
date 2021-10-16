33278ec1eab6a3438392df330760ef7470125d8d
zahmis <meda829ratteru86@gmail.com>
2021-10-16 23:02:45 +0900

    create iai

---
+++ iai.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char c;
+    cin >> c;
+    const string boin = "aiueo";
+    cout<< (boin.find(c) != string::npos ? "vowel" : "constant");
+
+    return 0;
+}

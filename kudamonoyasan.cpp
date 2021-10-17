6a8766c05be5c5baa5408b06a19a7e112a81e712
zahmis <meda829ratteru86@gmail.com>
2021-10-18 07:40:15 +0900

    fix segmentation fault

--- kudamonoyasan.cpp
+++ kudamonoyasan.cpp
@@ -16,7 +16,7 @@ int main() {
 
   int count = 0;
   for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; i++) {
+    for (int j = 0; j < N; j++) {
       if (A[i] + P[j] == S) {
         count = count + 1;
       }

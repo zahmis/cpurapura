b63c7b13cbdfff4c3c98e2401d046f48404faf01
zahmis <meda829ratteru86@gmail.com>
2022-05-31 12:16:29 +0900

    updateDirectory

---
+++ atCorder/warmUp/sample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    int value = 0;
+    cout << "Enter a value: "<< value <<endl;
+    return 0;
+}

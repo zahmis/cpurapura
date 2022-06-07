2735eb8bdec97e09a5a97ed4614ea01b4e77911b
zahmis <meda829ratteru86@gmail.com>
2022-06-08 07:04:20 +0900

    add

--- atCorder/problems/A/DigitMachine.cc
+++ atCorder/problems/A/DigitMachine.cc
@@ -2,4 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() { return 0; };
+int main() {
+  int a[10];
+
+  cin >> a[0] >> a[1] >> a[2] >> a[3] >> a[4] >> a[5] >> a[6] >> a[7] >> a[8] >>
+      a[9];
+
+  int first = a[a[0]];
+  int second = a[first];
+  cout << second << endl;
+
+  return 0;
+};
